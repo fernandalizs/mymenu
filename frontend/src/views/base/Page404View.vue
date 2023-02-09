c30b3f4af07c5d22f361e29575d5f0d273fecee5
Fernanda Liz <fernandaliz2004@hotmail.com>
2023-02-09 11:43:06 -0300

    arruma página de erro

--- frontend/src/views/base/Page404View.vue
+++ frontend/src/views/base/Page404View.vue
@@ -1,16 +1,27 @@
 <template>
   <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <v-card>
-        <v-card-title class="headline">Ops!</v-card-title>
-        <v-card-text> Não foi possível achar está página </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" :to="{ name: 'base-home' }"> Início </v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-responsive class="d-flex align-center text-center fill-height justify-center">
+      <h1 class="font-weight-thin">404</h1>
+      <h2>Page not found</h2>
+      <h4 class="pa-3 font-weight-medium">Ops! The page you are looking for does not exist</h4>
+      <v-container class="size">
+        <v-btn block color="#6CA858" :to="{ name: 'base-home' }" class="ma-3"> Home </v-btn>
+      </v-container>
     </v-responsive>
   </v-container>
 </template>
-
-<script setup></script>
+<style scoped>
+  h1 {
+    font-size: 20rem;
+    color: #507444;
+  }
+  h4 {
+    letter-spacing: 0.2em;
+  }
+  .v-btn {
+    color: white;
+  }
+  .size {
+    max-width: 30vw;
+  }
+</style>
